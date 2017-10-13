28aaba721a6e034a28e9b689d42d2242d6c72498
MYCOMPUTER\nwuser <taisei.rock.metal@gmail.com>
2017-10-13 11:38:07 +0900

    playerの移動制限

--- Assets/src/taisei/player.cs
+++ Assets/src/taisei/player.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class player : MonoBehaviour {
-
+	GameObject Player;
+	public bool isHitwall;
 	// Use this for initialization
 	void Start () {
-		
+		Player = GameObject.Find ("Galaga_OBJ_dualFighter_0");
 	}
-	
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKey(KeyCode.RightArrow)){
@@ -17,5 +17,12 @@ public class player : MonoBehaviour {
 		if (Input.GetKey (KeyCode.LeftArrow)) {
 			transform.position += new Vector3 (-0.1f, 0);
 		}
+		if (Input.GetKeyDown(KeyCode.RightArrow) && isHitwall==true ) 
+		{
+			GetComponent<Rigidbody2D>().AddForce( 
+				new Vector3(0, 300.0f, 0));
+			isHitwall = false;
+		}
+
 	}
 }
